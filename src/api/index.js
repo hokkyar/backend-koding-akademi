7f6576af4cdf42b6794f18c3d1bd55b0bc580226
CJackster <jinx7xsun@gmail.com>
2023-03-29 12:15:11 +0800

    update courses

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 router.use(require('./users/routes'))
-router.use(require('./products/routes'))
+router.use(require('./courses/routes'))
 
 module.exports = router
