ab1c2331be0d8d484a3fdf7f9a110a2425586634
hokkyar <hokkyaryasta@gmail.com>
2023-04-09 11:39:49 +0700

    deleting my-course

--- src/api/user/my-courses/MyCourseRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const asyncHandler = require('express-async-handler')
-
-const { getUserCourse } = require('./MyCourseControllers')
-
-router.get('/my-course', asyncHandler(getUserCourse))
-
-module.exports = router
