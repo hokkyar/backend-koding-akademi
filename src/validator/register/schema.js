2de7387ec4110a9b4b80cb21823d1627767c613a
hokkyar <hokkyaryasta@gmail.com>
2023-04-14 14:05:15 +0700

    update

--- src/validator/register/schema.js
+++ src/validator/register/schema.js
@@ -2,14 +2,17 @@ const Joi = require('joi')
 
 exports.RegisterSchema = Joi.object({
   email: Joi.string().required().email(),
-  password: Joi.string().required().min(8).regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]+$/).messages({
-    'string.pattern.base': 'Password must contain at least one letter, one number, and one symbol',
+  password: Joi.string().required().min(8).regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/).messages({
+    'string.pattern.base': 'Password must contain at least one letter and one number',
   }),
   confirmPassword: Joi.string().required().valid(Joi.ref('password')).messages({
     'any.only': 'Password do not match'
   }),
   full_name: Joi.string().required(),
-  phone_number: Joi.string().allow(null).pattern(/^(?:\+62|08)[0-9]{8,10}$/).messages({
+  // phone_number: Joi.string().allow(null).pattern(/^(?:\+62|08)[0-9]{8,10}$/).messages({
+  //   'string.pattern.base': 'Invalid phone number'
+  // })
+  phone_number: Joi.string().allow(null).pattern(/^(?:\+62|0)[1-9][0-9]{7,11}$/).messages({
     'string.pattern.base': 'Invalid phone number'
   })
 })
