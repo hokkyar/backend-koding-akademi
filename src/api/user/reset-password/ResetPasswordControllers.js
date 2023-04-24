579839adc4d0e10f9ded170e4dbdfadc29d576ae
hokkyar <hokkyaryasta@gmail.com>
2023-04-24 12:55:04 +0700

    reset-password update

--- src/api/user/reset-password/ResetPasswordControllers.js
+++ src/api/user/reset-password/ResetPasswordControllers.js
@@ -1,10 +1,8 @@
 const { resetPasswordService } = require('./ResetPasswordServices')
-const { validateResetPasswordBody } = require('../../../validator/reset-password')
 
 exports.resetPassword = async (req, res) => {
-  validateResetPasswordBody(req.body)
   const userId = req.user.id
-  await resetPasswordService(req.body, userId)
+  await resetPasswordService(userId)
   res.json({
     message: 'Reset password success'
   })
