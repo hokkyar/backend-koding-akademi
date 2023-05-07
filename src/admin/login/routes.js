edbaae36059b8627badd5a77dbeb6f3e2897d74d
hokkyar <hokkyaryasta@gmail.com>
2023-05-07 11:08:53 +0700

    admin panel login

--- src/admin/login/routes.js
+++ src/admin/login/routes.js
@@ -1,20 +1,28 @@
 const router = require('express').Router()
+const { Admin } = require('../../models/index')
+const bcrypt = require('bcrypt')
 
 router.get('/', (req, res) => {
   if (req.session.user) {
-    res.redirect('/admin/dashboard')
+    return res.redirect('/admin/dashboard')
   } else {
-    res.render('pages/login/page')
+    return res.render('pages/login/page', { message: null })
   }
 })
 
-router.post('/', (req, res) => {
-  if (req.body.username === 'admin' && req.body.password === 'admin') {
-    req.session.user = { username: req.body.username }
-    return res.redirect('/admin/dashboard')
-  } else {
-    return res.redirect('/admin/login')
-  }
+router.post('/', async (req, res) => {
+  const { username, password } = req.body
+  const adminAccount = await Admin.findOne({ where: { username } })
+
+  if (!adminAccount) return res.render('pages/login/page', { message: 'Invalid username or password' })
+
+  const hashedPassword = adminAccount.password
+  const isPasswordMatched = await bcrypt.compare(password, hashedPassword)
+
+  if (!isPasswordMatched) return res.render('pages/login/page', { message: 'Invalid username or password' })
+
+  req.session.user = { username }
+  return res.redirect('/admin/dashboard')
 })
 
 module.exports = router
