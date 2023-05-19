0d60e9f38b164d464cae71e319b55e6c4a16cc61
hokkyar <hokkyaryasta@gmail.com>
2023-05-19 14:48:41 +0700

    update

--- src/utils/sendNotification.js
+++ src/utils/sendNotification.js
@@ -1,26 +1,26 @@
-const admin = require('firebase-admin')
-const serviceAccount = require('./serviceAccountKey.json')
+// const admin = require('firebase-admin')
+// const serviceAccount = require('./serviceAccountKey.json')
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-})
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// })
 
-const sendNotification = () => {
-  const message = {
-    notification: {
-      title: 'Ada PROMO!',
-      body: 'Ayo segera beli',
-    },
-    topic: 'general'
-    // token: 'device_token',
-  }
-  admin.messaging().send(message)
-    .then((response) => {
-      console.log('Push notification successfully sent: ', response)
-    })
-    .catch((error) => {
-      console.error('An error occured: ', error)
-    })
-}
+// const sendNotification = () => {
+//   const message = {
+//     notification: {
+//       title: 'Ada PROMO!',
+//       body: 'Ayo segera beli',
+//     },
+//     topic: 'general'
+//     // token: 'device_token',
+//   }
+//   admin.messaging().send(message)
+//     .then((response) => {
+//       console.log('Push notification successfully sent: ', response)
+//     })
+//     .catch((error) => {
+//       console.error('An error occured: ', error)
+//     })
+// }
 
-module.exports = sendNotification
+// module.exports = sendNotification
