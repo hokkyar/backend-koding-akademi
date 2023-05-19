03f789fd219af23f19a4153f779881655076d293
hokkyar <hokkyaryasta@gmail.com>
2023-05-19 13:14:50 +0700

    update dashboard and qr-code

--- src/admin/dashboard/routes.js
+++ src/admin/dashboard/routes.js
@@ -1,14 +1,45 @@
 const router = require('express').Router()
+const { User, Product } = require('../../models/index')
 
 const params = {
   page: 'dashboard',
   sub_page: 'page',
   title: 'Dashboard',
-  sub: '',
-  detail: '',
-  data: ''
+  sub: null,
+  detail: null,
+  data: null
 }
 
-router.get('/', (req, res) => res.render('index', params))
+router.get('/', async (req, res) => {
+  const data = {
+    registered_user: await getRegisteredUser(),
+    product_count: {
+      coding: await getProductCountByCategory('cat-course-1'),
+      programming: await getProductCountByCategory('cat-course-2'),
+      engineering: await getProductCountByCategory('cat-course-3'),
+      bootcamp: await getProductCountByCategory('cat-course-4'),
+      robotic: await getProductCountByCategory('cat-course-5'),
+      event: await getProductCountByCategory('cat-event-1')
+    },
+  }
+  res.render('index', { ...params, data })
+})
+
+async function getRegisteredUser() {
+  const totalUser = await User.count({
+    where: { role: 'user' }
+  })
+  return totalUser
+}
+
+async function getProductCountByCategory(category_id) {
+  const totalProduct = await Product.count({
+    where: { category_id }
+  })
+  return totalProduct
+}
+
+// async function getSales(){}
+// async function getRevenue(){}
 
 module.exports = router
